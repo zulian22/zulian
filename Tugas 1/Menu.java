a0f8d99361a2bc436f374f8e78bf94668605e4b0
zulian22 <zulianfandi1@gmail.com>
2026-05-01 13:20:48 +0700

    Add files via upload

---
+++ Tugas 1/Menu.java
@@ -0,0 +1,12 @@
+
+class Menu {
+    String nama;
+    int harga;
+    String kategori;
+
+    Menu(String nama, int harga, String kategori) {
+        this.nama = nama;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+}
